6479c85639aed2397df3abf0f976ce4b3b878870
Dmitry Kisler <admin@dkisler.com>
2023-01-22 14:15:13 +0100

    Add docstring
    Signed-off-by: Dmitry Kisler <admin@dkisler.com>

--- plugin/neon/models.go
+++ plugin/neon/models.go
@@ -2,15 +2,22 @@ package neon
 
 // SecretAdmin defines the secret with the db admin access details.
 type SecretAdmin struct {
+	// Token Neon API token
 	Token string `json:"token"`
 }
 
 // SecretUser defines the secret with db user access details.
 type SecretUser struct {
-	User         string `json:"user"`
-	Password     string `json:"password"`
-	Host         string `json:"host"`
-	ProjectID    string `json:"project_id"`
-	BranchID     string `json:"branch_id"`
+	// User Neon role
+	User string `json:"user"`
+	// Password Neon role's access password
+	Password string `json:"password"`
+	// Host Neon endpoint URI to access database
+	Host string `json:"host"`
+	// ProjectID Neon project ID
+	ProjectID string `json:"project_id"`
+	// BranchID Neon branch ID
+	BranchID string `json:"branch_id"`
+	// DatabaseName Neon database name
 	DatabaseName string `json:"dbname"`
 }
